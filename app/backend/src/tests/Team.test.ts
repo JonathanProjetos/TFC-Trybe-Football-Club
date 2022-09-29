986b7589358c2455c58796b04f884b6ee319428f
Jonathan Santos <jonathan01347@gmail.com>
2022-09-29 19:26:46 -0300

    feat(test): Implementado test para a rota Team findAll/findOne.

--- app/backend/src/tests/Team.test.ts
+++ app/backend/src/tests/Team.test.ts
@@ -4,7 +4,7 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import Team from '../database/models/Team';
+import Team from '../database/models/TeamsModel';
 import MockTeam from '../tests/mock/MockTeam';
 
 import { Response } from 'superagent';
@@ -13,28 +13,49 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Testando end-point Team', () => {
+describe('Testando end-point Team findAll', () => {
   let chaiHttpResponse: Response;
 
   before(async () => {
-    sinon.stub(Team, "findAll").resolves(MockTeam as Team);
+    sinon.stub(Team, "findAll").resolves(MockTeam as Team[]);
   });
 
   after(()=>{
     (Team.findAll as sinon.SinonStub).restore();
   })
 
-  it('Testa o retorno bem sescedido', async () => {
+  it('Testa o retorno bem secedido', async () => {
     chaiHttpResponse = await chai
        .request(app)
        .get('/teams')
        .send(MockTeam)
 
     expect(chaiHttpResponse.status).to.equals(200)
-    expect(chaiHttpResponse.body).to.equals(MockTeam)
+    expect(chaiHttpResponse.body).to.be.not.empty;
   });
 
-  // it('Seu sub-teste', () => {
-  //   expect(false).to.be.eq(true);
-  // });
+});
+
+
+describe('Testando end-point Team findOne', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon.stub(Team, "findOne").resolves(MockTeam[0] as Team);
+  });
+
+  after(()=>{
+    (Team.findOne as sinon.SinonStub).restore();
+  })
+
+  it('Testa o retorno bem secedido', async () => {
+    chaiHttpResponse = await chai
+       .request(app)
+       .get('/teams/:id')
+       .send(MockTeam)
+
+    expect(chaiHttpResponse.status).to.equals(200)
+    expect(chaiHttpResponse.body).to.be.not.empty;
+  });
+
 });
