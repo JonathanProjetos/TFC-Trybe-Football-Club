779d9069ecf118368dd349de97c0329de9023b01
Jonathan Santos <jonathan01347@gmail.com>
2022-10-01 10:34:39 -0300

    feat(LeaderBoard): inciando a elaboração da rota leaderBoard

---
+++ app/backend/src/services/LeaderBoardHome.ts
@@ -0,0 +1,26 @@
+import Team from '../database/models/TeamsModel';
+import ILeaderBoard from '../interfaces/ILeaderBoard';
+
+class LeaderBoardHomeServices {
+  private db = Team;
+
+  LeaderBoardHome = async (): Promise<ILeaderBoard[]> => {
+    const team = await this.db.findAll();
+
+    const ObjectLeaderBoard = team.map((data) => ({
+      name: data.teamName,
+      totalPoints: 13,
+      totalGames: 5,
+      totalVictories: 4,
+      totalDraws: 1,
+      totalLosses: 0,
+      goalsFavor: 17,
+      goalsOwn: 5,
+      goalsBalance: 12,
+      efficiency: 86.67,
+    }));
+
+    return ObjectLeaderBoard as ILeaderBoard[];
+  };
+}
+export default LeaderBoardHomeServices;
