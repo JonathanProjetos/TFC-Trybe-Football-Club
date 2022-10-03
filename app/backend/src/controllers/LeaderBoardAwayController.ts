e981ee21721cea788a56e0adaf0b470073cd75f8
Jonathan Santos <jonathan01347@gmail.com>
2022-10-03 18:36:23 -0300

    feat(LeaderBoardAwayController): Implementado a controller da rota leaderBoard/away

--- app/backend/src/controllers/LeaderBoardAwayController.ts
+++ app/backend/src/controllers/LeaderBoardAwayController.ts
@@ -1,1 +1,14 @@
+import { Request, Response } from 'express';
+import IController from '../interfaces/IController';
+import LeaderBoardServices from '../services/LeaderBoardAwayServices';
 
+class LeaderBoardController implements IController {
+  constructor(private services: LeaderBoardServices) { }
+
+  LeaderBoardAway = async (req: Request, res: Response): Promise<Response> => {
+    const result = await this.services.LeaderBoardAway();
+    return res.status(200).json(result);
+  };
+}
+
+export default LeaderBoardController;
